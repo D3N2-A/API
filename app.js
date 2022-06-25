78b6e9bfced468aff46ad079db2ee609b471a226
D3N2-A <anmolsharma999937@gmail.com>
2022-06-25 10:32:22 +0530

    get and post method

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = reqiure("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,10 +9,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/articleDB");
+// <-----------------Schema-------------------->
+const articleSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+});
+const article = mongoose.model("article", articleSchema);
+// <----------------Article get--------------------------->
 
-// <--------------------------------------------------->
+app.get("/articles", (req, res) => {
+  article.find({}, (err, foundArticles) => {
+    res.send(foundArticles);
+  });
+});
 
-// <--------------------------------------------------->
+app.get("/articles/:articleName", (req, res) => {
+  const articleName = req.params.articleName;
+  article.find({}, (err, foundArticle) => {
+    foundArticle.forEach((el) => {
+      if (articleName === el.name) {
+        res.send(el);
+      } else {
+        res.send("Article not found");
+      }
+    });
+  });
+});
+
+// <----------------Article Post--------------------------->
+
+app.post("/articles", (req, res) => {
+  const name = req.body.title;
+  const content = req.body.content;
+  console.log(name);
+  console.log(content);
+});
+
+// <-------------------server listen------------------------>
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
